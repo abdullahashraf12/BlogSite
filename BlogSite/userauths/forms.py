1cd40669b18922164add28c25c9df52e69a06f08
unknown <alpha1.abdullah@gmail.com>
2024-06-26 03:49:15 +0300

    Registeration & Login Form UI Improvement

--- BlogSite/userauths/forms.py
+++ BlogSite/userauths/forms.py
@@ -32,8 +32,8 @@ class UserRegistrationForm(UserCreationForm):
             raise forms.ValidationError('This email address is already in use.')
         return email
 class UserLoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
     def clean(self):
         cleaned_data = super().clean()
